44e8ff7fe1c93b6492366abfafe069abe9633ef8
zhangL2auto <deutschlei47@126.com>
2023-04-07 15:20:38 +0800

    ma_control: first update

---
+++ ma_first.py
@@ -0,0 +1,10 @@
+'''
+Author: zhangL
+Date: 2023-04-07 15:19:59
+LastEditors: zhangL
+LastEditTime: 2023-04-07 15:20:19
+FilePath: /ma_control/ma_first.py
+'''
+import numpy as np
+
+print(np.__version__)
